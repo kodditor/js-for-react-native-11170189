186bf1903278d0aa04ccac414398338f30c45cb6
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-05-17 18:10:11 +0000

    chore: completed task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,3 +5,16 @@ function processArray(numArray){
     }
     return newArr;
 }
+
+function formatArrayStrings(stringArr, numArr){
+    for(let i = 0; i < stringArr.length; i++){
+        if(numArr[i] % 2 === 0){
+            //stringArr[i] = stringArr[i].toLocaleUpperCase(); // or capitalize? see: https://en.wikipedia.org/wiki/Capitalization
+            const tempString = stringArr[i].toLocaleLowerCase()
+            stringArr[i] = tempString[0].toLocaleUpperCase() + tempString.substring(1);
+        } else {
+            stringArr[i] = stringArr[i].toLocaleLowerCase();
+        }
+    }
+    return stringArr;
+}
